b37277ef72b516b57476bce1b0c2918bbba1c272
Aanand-Patil <anandpatil6440@gmail.com>
2025-05-19 18:13:05 +0530

    dummy data added

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,4 +1,33 @@
+import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react'
 import React from 'react'
+
+const items = [
+  {
+    title: 'Home',
+    url: '/',
+    icon: Home,
+  },
+  {
+    title: 'Inbox',
+    url: '#',
+    icon: Inbox,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+  {
+    title: 'Search',
+    url: '#',
+    icon: Search,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+]
 
 const AppSidebar = () => {
   return <div>AppSidebar</div>
