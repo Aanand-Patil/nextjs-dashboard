d09b17b6766120a4696a5a52bd58c9736a9989b1
Aanand-Patil <anandpatil6440@gmail.com>
2025-05-19 17:45:14 +0530

    theme provider created.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
       >
         <AppSidebar />
-        <main className=''>
+        <main className='w-full'>
           <Navbar />
           <div className='px-4'>{children}</div>
         </main>
